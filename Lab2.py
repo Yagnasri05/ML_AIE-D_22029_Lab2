947e0ff593454684383849a2343194044421bab4
Yagnasri05 <132754311+Yagnasri05@users.noreply.github.com>
2024-02-06 14:46:17 +0530

    Add files via upload

--- Lab2.py
+++ Lab2.py
@@ -22,6 +22,18 @@ def Manhattan_distance(v1,v2):
     return man_dist
 
 #Question 2
+def KNN(dt, k, test):
+    prediction = []
+    distances = []
+
+    for j in range(len(dt)):
+        vec = [dt[j][0], dt[j][1]]
+        d = Euclidean_distance(vec, test)
+        distances.append((d, dt[j][2]))
+
+    distances.sort(key=lambda x: x[0])
+    nearest_neighbors = distances[:k]
+    return nearest_neighbors
 
 #Question 3 
 def label_encoding(inp):
@@ -29,17 +41,17 @@ def label_encoding(inp):
     label={} # creating the dictionary to save the corresponding numeric value to the data
     dt=[] # To store converted Numerical data
     for i,c in enumerate(category):
-        label[c]=i
+        label[c]=i #assigning the numerical value to the data
     for i in inp:
         dt.append(label[i])
     return dt
 
 #Question 4
 def onehot_encode(inp,data):
-    onehot=[]
+    onehot=[] #empty dict for one hot encoding 
     for i in inp:
         if  data== i:
-            onehot.append(1)
+            onehot.append(1) #if entered data by user is equal to the data present, then it is given as 1 else, it is given as 0
         else:
             onehot.append(0)   
     return onehot
@@ -62,3 +74,14 @@ print(ans3)
 data=input("Enter the word to check:")
 ans4=onehot_encode(inp,data)
 print(ans4)
+
+#Ans 2
+d1=[110,150,175,195,95]
+d2=[1000,2500,3500,2750,1100]
+d3=[1,1,0,1,0]
+dt=[d1,d2,d3] #dataset to find the nearest neighbours
+k=int(input("Enter number of neighbour: "))
+vector_data=input("Enter the vectors with space : ")
+test = list(map(int, vector_data.split()))
+KNN_classifier=KNN(dt,k,test)
+print(KNN_classifier)
